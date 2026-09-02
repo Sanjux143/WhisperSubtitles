a59fa1bf4a66ceb7135d416b15e1b1b3ccc9c35d
Sanjux143 <208314328+Sanjux143@users.noreply.github.com>
2026-09-02 11:20:02 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,57 +4,43 @@ plugins {
 }
 
 android {
-
     namespace = "com.example.whispersubtitles"
-
     compileSdk = 35
-
     ndkVersion = "27.0.12077973"
 
     defaultConfig {
-
         applicationId = "com.example.whispersubtitles"
-
         minSdk = 26
-
         targetSdk = 35
-
         versionCode = 1
-
         versionName = "1.0"
 
         externalNativeBuild {
-
             cmake {
-
                 cppFlags += "-std=c++17"
-
             }
-
         }
+    }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 
     externalNativeBuild {
-
         cmake {
-
             path = file("src/main/cpp/CMakeLists.txt")
-
         }
-
     }
-
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.15.0")
-
     implementation("androidx.appcompat:appcompat:1.7.0")
-
     implementation("com.google.android.material:material:1.12.0")
-
     implementation("androidx.cardview:cardview:1.0.0")
-
 }
